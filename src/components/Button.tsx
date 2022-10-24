0566b7965aa0c21603c9a064a6fdb5e9eabc756e
Aen Jojo <josuafernando999@gmail.com>
2022-10-24 16:54:37 +0700

    feat: add button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,32 @@
+import type { ReactElement } from 'react';
+import clsx from 'clsx';
+import styles from '#styles/Button.module.css';
+
+interface ButtonPropsType {
+	value: string,
+	iconLeft?: ReactElement,
+	iconRight?: ReactElement,
+	type: 'fill' | 'outline' | 'empty',
+	color: 'primary' | 'secondary',
+};
+
+export default function Button({ value, iconLeft, iconRight, type, color }: ButtonPropsType) {
+	const LinkButtonStyle = clsx([
+		styles.button,
+		type === 'fill' && styles.fill,
+		type === 'outline' && styles.outline,
+		type === 'empty' && styles.empty,
+		color === 'primary' && styles.primary,
+		color === 'secondary' && styles.secondary,
+	]);
+	
+	return (
+		<button
+			className={LinkButtonStyle}
+		>
+			{iconLeft && <span>{iconLeft}</span>}
+			<span>{value}</span>
+			{iconRight && <span>{iconRight}</span>}
+		</button>
+	);
+};

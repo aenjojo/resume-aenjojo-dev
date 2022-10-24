8a53af22a2615636a041984c51f340bc57070c12
Aen Jojo <josuafernando999@gmail.com>
2022-10-24 16:58:25 +0700

    refactor: add index file for heading directory
    * refactor: add index file for heading components directory
    
    * refactor: add index file for link components directory

---
+++ src/components/Heading/index.ts
@@ -0,0 +1,11 @@
+import MainTitle from './MainTitle';
+import SectionTitle from './SectionTitle';
+import SubSectionTitle from './SubSectionTitle';
+import GenericTitle from './GenericTitle';
+
+export {
+	MainTitle,
+	SectionTitle,
+	SubSectionTitle,
+	GenericTitle,
+};
---
+++ src/components/Link/index.ts
@@ -0,0 +1,9 @@
+import ButtonLink from './ButtonLink';
+import TextLink from './TextLink';
+import BaseLink from './BaseLink';
+
+export {
+	ButtonLink,
+	TextLink,
+	BaseLink,
+};

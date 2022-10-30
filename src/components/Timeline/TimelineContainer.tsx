650bf67e8f16cda20058fd396b2035caa07e464e
Aen Jojo <josuafernando999@gmail.com>
2022-10-31 00:13:12 +0700

    feat: add timeline

---
+++ src/components/Timeline/TimelineContainer.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from "react";
+
+interface TimelineContainerPropsType {
+	children: ReactNode | ReactNode[],
+}
+
+export default function TimelineContainer({ children }: TimelineContainerPropsType) {
+	return (
+		<div
+			className='flex flex-col-reverse gap-0 mx-4 py-2'
+		>
+			{children}
+		</div>
+	);
+}

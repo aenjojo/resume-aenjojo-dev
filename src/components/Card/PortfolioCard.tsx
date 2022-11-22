9e17f6b09c608d862d69ecb320d9d9698049c178
Aen Jojo <josuafernando999@gmail.com>
2022-11-22 23:06:19 +0700

    feat: make image responsive with different size
    * feat: make image responsive with different size
    
    * refactor: remove tags `chip`
    
    * refactor: change `info` button to `detail` with its icon
    
    * style: separate attribute into each line instead of inline

--- src/components/Card/PortfolioCard.tsx
+++ src/components/Card/PortfolioCard.tsx
@@ -2,8 +2,7 @@ import Image from 'next/image';
 import { CardContainer } from './CardContainer';
 import { GenericTitle } from '#components/Heading';
 import { ButtonLink } from '#components/Link';
-import { Chip } from '#components/Chip';
-import { MdOutlineCode, MdInfoOutline, MdOutlinePublic } from 'react-icons/md';
+import { MdOutlineCode, MdOutlineArticle, MdOutlinePublic } from 'react-icons/md';
 
 export interface PortfolioCardPropsType {
 	imageSource: string,
@@ -15,26 +14,49 @@ export interface PortfolioCardPropsType {
 	codeUrl?: string,
 }
 
-// make sure image has a same size and aspect-ratio
-
-export function PortfolioCard({ imageSource, imageAlt, tags, title, description, projectUrl, codeUrl }: PortfolioCardPropsType) {
+export function PortfolioCard({ imageSource, imageAlt, title, description, projectUrl, codeUrl }: PortfolioCardPropsType) {
 	return (
 		<CardContainer>
-			<Image src={imageSource} alt={imageAlt} width={1024} height={1024} />
-			<section className='flex flex-col gap-4 p-4 border-t border-primary-200 h-full'>
-				<div className='flex flex-row flex-wrap gap-2'>
-					{tags.map(tag => (
-						<Chip key={tag} value={tag} />
-					))}
-				</div>
+			<section className='block relative overflow-hidden h-48'>
+				<Image
+					src={imageSource}
+					alt={imageAlt}
+					fill={true}
+					className='object-cover'
+					priority={false}
+					sizes='(min-width: 1536) 25vw, (min-width: 1024) 33vw, (min-width: 768px) 50vw, 100vw'
+				/>
+			</section>
+			<section className='flex flex-col gap-4 p-4 border-t border-primary-200 h-48'>
 				<div className='flex-grow'>
 					<GenericTitle>{title}</GenericTitle>
 					<p>{description}</p>
 				</div>
 				<div className='flex flex-row gap-2 justify-end'>
-					<ButtonLink value='Info' href={projectUrl} variant='empty' color='primary' icon={<MdInfoOutline />} className='justify-center' />
-					{codeUrl && <ButtonLink value='Code' href={codeUrl} variant='empty' color='primary' icon={<MdOutlineCode />} className='justify-center' />}
-					<ButtonLink value='Demo' href={projectUrl} variant='empty' color='primary' icon={<MdOutlinePublic />} className='justify-center' />
+					<ButtonLink
+						value='Detail'
+						href={projectUrl}
+						variant='empty'
+						color='primary'
+						icon={<MdOutlineArticle size={20} />}
+						className='justify-center'
+					/>
+					{codeUrl && <ButtonLink
+						value='Code'
+						href={codeUrl}
+						variant='empty'
+						color='primary'
+						icon={<MdOutlineCode size={20} />}
+						className='justify-center'
+					/>}
+					<ButtonLink
+						value='Demo'
+						href={projectUrl}
+						variant='empty'
+						color='primary'
+						icon={<MdOutlinePublic size={20} />}
+						className='justify-center'
+					/>
 				</div>
 			</section>
 		</CardContainer>

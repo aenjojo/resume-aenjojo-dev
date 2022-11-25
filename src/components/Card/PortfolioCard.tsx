a21568e50c1acc97fc68657d22dfe8bca2a02156
Aen Jojo <josuafernando999@gmail.com>
2022-11-25 14:51:10 +0700

    refactor: change `projectUrl` to `demoUrl`
    * refactor: change `projectUrl` to `demoUrl`
    
    * feat: add ability to construct page link with `title` value
    
    * fix: add missing `px` inside image sizes
    
    * feat: add link to card title
    
    * feat: remove detail button
    
    * refactor: change demoUrl as optional
    
    * feat: change demo button as optional

--- src/components/Card/PortfolioCard.tsx
+++ src/components/Card/PortfolioCard.tsx
@@ -1,19 +1,23 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import { CardContainer } from './CardContainer';
 import { GenericTitle } from '#components/Heading';
 import { ButtonLink } from '#components/Link';
 import { MdOutlineCode, MdOutlineArticle, MdOutlinePublic } from 'react-icons/md';
+import styles from '#styles/Link.module.css';
+import clsx from 'clsx';
 
 export interface PortfolioCardPropsType {
-	imageSource: string,
-	tags: string[],
 	title: string,
 	description: string,
-	projectUrl: string,
+	imageSource: string,
+	demoUrl?: string,
 	codeUrl?: string,
 }
 
-export function PortfolioCard({ imageSource, title, description, projectUrl, codeUrl }: PortfolioCardPropsType) {
+export function PortfolioCard({ title, description, imageSource, demoUrl, codeUrl }: PortfolioCardPropsType) {
+	const detailPageUrl = '/projects/'+title.toLowerCase().replaceAll(' ', '-');
+
 	return (
 		<CardContainer>
 			<section className='block relative overflow-hidden h-48'>
@@ -23,23 +27,20 @@ export function PortfolioCard({ imageSource, title, description, projectUrl, cod
 					fill={true}
 					className='object-cover'
 					priority={false}
-					sizes='(min-width: 1536) 25vw, (min-width: 1024) 33vw, (min-width: 768px) 50vw, 100vw'
+					sizes='(min-width: 1536px) 25vw, (min-width: 1024px) 33vw, (min-width: 768px) 50vw, 100vw'
 				/>
 			</section>
 			<section className='flex flex-col gap-4 p-4 border-t border-primary-200 h-48'>
 				<div className='flex-grow'>
-					<GenericTitle>{title}</GenericTitle>
+					<Link
+						href={detailPageUrl}
+						className={styles.base}
+					>
+						<GenericTitle>{title}</GenericTitle>
+					</Link>
 					<p>{description}</p>
 				</div>
 				<div className='flex flex-row gap-2 justify-end'>
-					<ButtonLink
-						value='Detail'
-						href={projectUrl}
-						variant='empty'
-						color='primary'
-						icon={<MdOutlineArticle size={20} />}
-						className='justify-center'
-					/>
 					{codeUrl && <ButtonLink
 						value='Code'
 						href={codeUrl}
@@ -48,14 +49,14 @@ export function PortfolioCard({ imageSource, title, description, projectUrl, cod
 						icon={<MdOutlineCode size={20} />}
 						className='justify-center'
 					/>}
-					<ButtonLink
+					{demoUrl && <ButtonLink
 						value='Demo'
-						href={projectUrl}
+						href={demoUrl}
 						variant='empty'
 						color='primary'
 						icon={<MdOutlinePublic size={20} />}
 						className='justify-center'
-					/>
+					/>}
 				</div>
 			</section>
 		</CardContainer>

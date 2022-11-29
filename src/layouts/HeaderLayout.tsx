c5634cee3acb7783f7514bf42bc5fbaa1523bd94
Aen Jojo <josuafernando999@gmail.com>
2022-11-29 23:57:32 +0700

    refactor: remove unnecessary `react fragment`
    * refactor: remove unnecessary `react fragment`
    
    * refactor: rename `portfolio` navigation to `projects`
    
    * feat: use `<DisplayText />` for main logo text
    
    * refactor: remove unnecessary typescript interface
    
    * feat: change background color with gradient
    
    * feat: add border on bottom

--- src/layouts/HeaderLayout.tsx
+++ src/layouts/HeaderLayout.tsx
@@ -1,19 +1,16 @@
 import { Button } from '#components/Button';
 import { ButtonLink } from '#components/Link';
-import { MdMenu, MdClose } from 'react-icons/md';
+import { DisplayText } from '#components/Text';
+import { MdOutlineMenu, MdOutlineClose } from 'react-icons/md';
 import { useState } from 'react';
 import clsx from 'clsx';
 
-interface HeaderLayoutType {
-	//
-};
-
-export default function HeaderLayout({ }: HeaderLayoutType) {
+export default function HeaderLayout() {
 	const [isOpen, setIsOpen] = useState(false);
 
 	const NavigationList = [
 		'Resume',
-		'Portfolio',
+		'Projects',
 		'Blog'
 	].map(element => (
 		<ButtonLink
@@ -27,36 +24,37 @@ export default function HeaderLayout({ }: HeaderLayoutType) {
 	))
 
 	return (
-		<>
-			<header className='bg-base-100 border-b border-b-base-300'>
-				<section className='container mx-auto flex flex-row justify-between px-2 py-1'>
-					<div className='flex flex-row flex-nowrap p-2'>
-						<span className='text-2xl font-bold text-secondary-500'>Aen</span>
-						<span className='text-2xl font-bold text-primary-500'>Jojo</span>
+		<header className='bg-gradient-to-r from-primary-100 to-secondary-100 border-b border-b-primary-200'>
+			<section className='container mx-auto flex flex-row justify-between px-2 py-1'>
+					<div className='flex flex-row justify-center items-center'>
+						<DisplayText value='Aen' size='XXL' className='text-center text-secondary-500 inline-block' />
+						<DisplayText value='Jojo' size='XXL' className='text-center text-primary-500 inline-block' />
 					</div>
-					<nav className='hidden lg:flex lg:flex-row'>
-						{NavigationList}
-					</nav>
-					<section className='flex lg:hidden'>
-						<Button
-							value='Menu'
-							isSrOnly={true}
-							icon={isOpen ? <MdClose size={24} /> : <MdMenu size={24} />}
-							variant='fill'
-							color={isOpen ? 'secondary' : 'primary'}
-							onclick={() => setIsOpen(!isOpen)}
-						/>
-					</section>
-				</section>
-				<nav
-					className={clsx([
-						'flex flex-col lg:hidden w-full transition-all duration-300',
-						isOpen ? 'h-auto' : 'h-0 hidden'
-					])}
-				>
+				<nav className='hidden lg:flex lg:flex-row'>
 					{NavigationList}
 				</nav>
-			</header>
-		</>
+				<section className='flex lg:hidden'>
+					<Button
+						value='Menu'
+						icon={isOpen
+							? <MdOutlineClose size={24} />
+							: <MdOutlineMenu size={24} />
+						}
+						iconOnly
+						variant='fill'
+						color={isOpen ? 'secondary' : 'primary'}
+						onclick={() => setIsOpen(!isOpen)}
+					/>
+				</section>
+			</section>
+			<nav
+				className={clsx([
+					'flex flex-col lg:hidden w-full transition-all duration-300',
+					isOpen ? 'h-auto' : 'h-0 hidden'
+				])}
+			>
+				{NavigationList}
+			</nav>
+		</header>
 	);
 };

8a9d794f8ec6b3c39508172e6b246d6fadda99a8
Aen Jojo <josuafernando999@gmail.com>
2022-12-04 23:58:07 +0700

    feat: add type export from heading

--- src/components/Heading/index.ts
+++ src/components/Heading/index.ts
@@ -2,3 +2,5 @@ export { MainTitle } from './MainTitle';
 export { SectionTitle } from './SectionTitle';
 export { SubSectionTitle } from './SubSectionTitle';
 export { GenericTitle } from './GenericTitle';
+
+export type { GenericTitlePropsType } from './GenericTitle';

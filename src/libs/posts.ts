93a04cf438a40ba90673a970c271e8299036aad6
Aen Jojo <josuafernando999@gmail.com>
2022-12-08 18:07:33 +0700

    refactor: add types

--- src/libs/posts.ts
+++ src/libs/posts.ts
@@ -1,12 +1,24 @@
 import { readdir } from 'node:fs/promises';
 import { resolve } from 'node:path';
 
+export interface metaDataType {
+	title: string,
+	excerpt: string,
+	date: string,
+	tags: string[],
+}
+
+export interface importType {
+	default: any,
+	meta: metaDataType,
+}
+
 export async function getPosts() {
 	const postsDirectory = resolve(process.cwd(), 'src/pages/blog/posts');
 	const filenames = await readdir(postsDirectory);
 
 	const postsDataPromises = filenames.map(async filename => {
-		const mdxFile = await import(`./../pages/blog/posts/${filename}`);
+		const mdxFile: importType = await import(`./../pages/blog/posts/${filename}`);
 
 		const postId = filename.replace(/.mdx$/, '');
 		const postMeta = mdxFile.meta;

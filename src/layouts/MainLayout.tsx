cf5a1f7b3be08397300b843f5d0ada755f88c8a6
Aen Jojo <josuafernando999@gmail.com>
2022-12-08 18:08:10 +0700

    refactor: change header and footer import

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,6 +1,5 @@
 import type { ReactNode } from 'react';
-import FooterLayout from './FooterLayout';
-import HeaderLayout from './HeaderLayout';
+import { Header, Footer } from '#components/Navigation';
 
 interface MainLayoutType {
 	children: ReactNode | ReactNode[],
@@ -9,11 +8,11 @@ interface MainLayoutType {
 export default function MainLayout({ children }: MainLayoutType) {
 	return (
 		<>
-			<HeaderLayout />
+			<Header />
 			<main className='container mx-auto my-4 p-4'>
 				{children}
 			</main>
-			<FooterLayout />
+			<Footer />
 		</>
 	);
 };

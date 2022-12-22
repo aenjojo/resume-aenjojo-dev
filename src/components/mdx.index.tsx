52ef12e2dc6bd26c58072ae2d373e2ef8fa60c64
Aen Jojo <josuafernando999@gmail.com>
2022-12-22 23:38:48 +0700

    feat: add withMargin prop to heading component

--- src/components/mdx.index.tsx
+++ src/components/mdx.index.tsx
@@ -4,10 +4,10 @@ import { TextLink } from '#components/Link';
 
 export const components = {
 	// heading
-	h1: ({ children }) => <MainTitle>{children as string}</MainTitle>,
-	h2: ({ children }) => <SectionTitle>{children as string}</SectionTitle>,
-	h3: ({ children }) => <SubSectionTitle>{children as string}</SubSectionTitle>,
-	h4: ({ children }) => <GenericTitle>{children as string}</GenericTitle>,
+	h1: ({ children }) => <MainTitle withMargin>{children as string}</MainTitle>,
+	h2: ({ children }) => <SectionTitle withMargin>{children as string}</SectionTitle>,
+	h3: ({ children }) => <SubSectionTitle withMargin>{children as string}</SubSectionTitle>,
+	h4: ({ children }) => <GenericTitle withMargin>{children as string}</GenericTitle>,
 	
 	// link
 	a: ({ href, children }) => <TextLink href={href as string} value={children as string} />,

425987b138972e09a60c7e9af71e8d1248e888cc
Aen Jojo <josuafernando999@gmail.com>
2022-12-27 16:49:41 +0700

    style: wrap component for better readability

--- src/components/mdx.index.tsx
+++ src/components/mdx.index.tsx
@@ -4,11 +4,32 @@ import { TextLink } from '#components/Link';
 
 export const components = {
 	// heading
-	h1: ({ children }) => <MainTitle withMargin>{children as string}</MainTitle>,
-	h2: ({ children }) => <SectionTitle withMargin>{children as string}</SectionTitle>,
-	h3: ({ children }) => <SubSectionTitle withMargin>{children as string}</SubSectionTitle>,
-	h4: ({ children }) => <GenericTitle withMargin>{children as string}</GenericTitle>,
+	h1: ({ children }) => (
+		<MainTitle withId withMargin>
+			{children as string}
+		</MainTitle>
+	),
+	h2: ({ children }) => (
+		<SectionTitle withId withMargin>
+			{children as string}
+		</SectionTitle>
+	),
+	h3: ({ children }) => (
+		<SubSectionTitle withId withMargin>
+			{children as string}
+		</SubSectionTitle>
+	),
+	h4: ({ children }) => (
+		<GenericTitle withId withMargin>
+			{children as string}
+		</GenericTitle>
+	),
 	
 	// link
-	a: ({ href, children }) => <TextLink href={href as string} value={children as string} />,
+	a: ({ href, children }) => (
+		<TextLink
+			href={href as string}
+			value={children as string}
+		/>
+	),
 } as MDXComponents;

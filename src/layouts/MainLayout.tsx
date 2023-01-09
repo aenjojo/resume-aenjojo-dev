54e58da5ab7d02881aff4a0dc106578468edf7b7
Aen Jojo <josuafernando999@gmail.com>
2023-01-09 23:57:25 +0700

    feat: customizable display between grid and none
    * feat: customizable display between grid and none
    
    * refactor: use clsx to maintain different style between grid and none

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,15 +1,22 @@
 import type { ReactNode } from 'react';
 import { Header, Footer } from '#components/Navigation';
+import clsx from 'clsx';
 
 interface MainLayoutType {
 	children: ReactNode | ReactNode[],
+	display?: 'grid',
 }
 
-export default function MainLayout({ children }: MainLayoutType) {
+export function MainLayout({ children, display }: MainLayoutType) {
+	const MainLayoutStyle = clsx([
+		'container mx-auto pt-4 pb-10 px-4 lg:px-40',
+		display === 'grid' ? 'grid grid-cols-4 md:grid-cols-8 lg:grid-cols-12 gap-4' : '',
+	]);
+
 	return (
 		<>
 			<Header />
-			<main className='container mx-auto p-4'>
+			<main className={MainLayoutStyle}>
 				{children}
 			</main>
 			<Footer />

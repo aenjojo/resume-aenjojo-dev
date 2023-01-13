1958b64d3bbc3180e4f47021b6fb15ec23766f50
Aen Jojo <josuafernando999@gmail.com>
2023-01-13 23:58:59 +0700

    feat: add SEO component as props for main layout

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,13 +1,15 @@
 import type { ReactNode } from 'react';
 import { Header, Footer } from '#components/Navigation';
+import { SEO, type SEOPropsType } from '#components/Head';
 import clsx from 'clsx';
 
 interface MainLayoutType {
 	children: ReactNode | ReactNode[],
 	display?: 'grid',
+	seo: SEOPropsType,
 }
 
-export function MainLayout({ children, display }: MainLayoutType) {
+export function MainLayout({ children, display, seo }: MainLayoutType) {
 	const MainLayoutStyle = clsx([
 		'container mx-auto pt-4 pb-10 px-4 lg:px-40',
 		display === 'grid' ? 'grid grid-cols-4 md:grid-cols-8 lg:grid-cols-12 gap-4' : '',
@@ -15,6 +17,7 @@ export function MainLayout({ children, display }: MainLayoutType) {
 
 	return (
 		<>
+			<SEO {...seo} />
 			<Header />
 			<main className={MainLayoutStyle}>
 				{children}

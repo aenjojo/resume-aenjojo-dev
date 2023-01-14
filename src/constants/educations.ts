9628701fe19ece054b181502601774a93d21fb7c
Aen Jojo <josuafernando999@gmail.com>
2023-01-14 23:35:32 +0700

    feat: add data for resume
    * feat: add data for resume
    
    * fix: add missing path thumbnail for site data
    
    * fix: remove trailing slash at website name

---
+++ src/constants/educations.ts
@@ -0,0 +1,13 @@
+export type educationsType = {
+	studyPlace: string,
+	studyFocus: string,
+	startDate: string,
+	endDate: string,
+};
+
+export const educations: educationsType[] = [{
+	studyPlace: 'STIKOM Tunas Bangsa',
+	studyFocus: 'Computer Science',
+	startDate: 'September 2018',
+	endDate: 'October 2022',
+}];
--- src/constants/sites.ts
+++ src/constants/sites.ts
@@ -1,5 +1,5 @@
 export const sites = {
 	siteName: 'Aen Jojo',
-	siteUrl: 'https://aenjojo.dev/',
-	siteThumbnail: '',
+	siteUrl: 'https://aenjojo.dev',
+	siteThumbnail: '/images/logo/AenJojo.png',
 };

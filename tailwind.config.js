a42d0c9e16ff4517c51c46655d107f680591af3b
Aen Jojo <josuafernando999@gmail.com>
2023-02-14 18:50:19 +0700

    chore: remove unused state color

--- tailwind.config.js
+++ tailwind.config.js
@@ -44,26 +44,6 @@ module.exports = {
         800: '#30363B',
         900: '#181C1F',
       },
-      'info': {
-        light: '#C8DFF8',
-        main: '#3875EA',
-        dark: '#0D2759',
-      },
-      'success': {
-        light: '#C9F5D8',
-        main: '#23B93A',
-        dark: '#0C4014',
-      },
-      'warning': {
-        light: '#F6F6E8',
-        main: '#CADD32',
-        dark: '#2B2C07',
-      },
-      'error': {
-        light: '#FAD8D8',
-        main: '#DC2E2E',
-        dark: '#570F0F',
-      },
     },
     extend: {
       fontFamily: {

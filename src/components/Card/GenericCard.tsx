328e6673979eb39d1986a5110becdf0a92155052
Aen Jojo <josuafernando999@gmail.com>
2023-02-16 22:01:33 +0700

    feat: add icon and some style to generic card

--- src/components/Card/GenericCard.tsx
+++ src/components/Card/GenericCard.tsx
@@ -1,17 +1,26 @@
-import { GenericTitle } from '..';
+import type { ReactNode } from 'react';
 import { CardContainer } from './CardContainer';
+import { GenericTitle } from '#components/Heading';
 
 export interface GenericCardPropsType {
 	title: string,
 	description: string,
+	icon?: ReactNode,
 }
 
-export function GenericCard({ title, description }: GenericCardPropsType) {
+export function GenericCard({ title, description, icon }: GenericCardPropsType) {
 	return (
 		<CardContainer>
-			<section className='h-auto p-4'>
-				<GenericTitle>{title}</GenericTitle>
-				<p>{description}</p>
+			<section className='h-auto p-4 flex flex-row lg:flex-col gap-4 items-start'>
+				{icon && (
+					<div className='rounded-full border border-primary-300 p-2'>
+						{icon}
+					</div>
+				)}
+				<div>
+					<GenericTitle>{title}</GenericTitle>
+					<p>{description}</p>
+				</div>
 			</section>
 		</CardContainer>
 	);

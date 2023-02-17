2187a5ea471e0c67cf3058de524f03bf3f511b9a
Aen Jojo <josuafernando999@gmail.com>
2023-02-17 18:22:00 +0700

    fix: reduce the padding and border radius

--- src/components/Chip/Chip.tsx
+++ src/components/Chip/Chip.tsx
@@ -5,7 +5,7 @@ export interface ChipPropsType {
 export function Chip({ value }: ChipPropsType) {
 	return (
 		<div
-			className='bg-primary-500 text-primary-100 rounded-full inline-block border border-primary-300 px-3 text-xs'
+			className='bg-primary-500 text-primary-100 rounded inline-block border border-primary-300 px-2 text-xs'
 		>
 			{value}
 		</div>

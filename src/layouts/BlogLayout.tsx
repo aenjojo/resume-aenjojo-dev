0fb3dbe3db588181df732e6563f5e00105bf1272
Aen Jojo <josuafernando999@gmail.com>
2023-02-23 19:01:38 +0700

    feat: update blog page layout
    * add breadcrumb navigation
    
    * add SEO metadata setter from main layout
    
    * add reading time function
    
    * add `main-content` class to set content styling
    
    * set image sizes
    
    * add share button

--- src/layouts/BlogLayout.tsx
+++ src/layouts/BlogLayout.tsx
@@ -1,18 +1,24 @@
 import Image from 'next/image';
-import type { ReactNode } from 'react';
+import { ReactNode, useEffect, useRef, useState } from 'react';
 import type { metaDataType } from '#libs/posts';
 import { MainLayout } from './MainLayout';
 import { MainTitle, SectionTitle } from '#components/Heading';
 import { DisplayText } from '#components/Text';
-import { ButtonLink } from '#components/Link';
 import { PostCard } from '#components/Card';
-import { MdChevronLeft } from 'react-icons/md';
-import { FaFacebook, FaInstagram, FaTwitter, FaWhatsapp, FaShare } from 'react-icons/fa';
+import { Breadcrumb } from '#components/Navigation';
+import { MdShare } from 'react-icons/md';
+import { useRouter } from 'next/router'
+import { calculateReadTime } from '#libs/calculateReadTime';
+import { RWebShare } from 'react-web-share';
+import { Button } from '#components/Button';
+import { sites } from '#constants/sites';
 
 interface BlogLayoutType {
 	children: ReactNode | ReactNode[],
 	metadata: {
 		title: string,
+		description: '',
+		keywords: [''],
 		date: string,
 		image?: string,
 		tags?: string[],
@@ -24,16 +30,45 @@ interface BlogLayoutType {
 }
 
 export function BlogLayout({ children, metadata, articles }: BlogLayoutType) {
+	const { pathname } = useRouter();
+	const contentRef = useRef<any>();
+	const [readTime, setReadTime] = useState(0);
+
+	const pathnameArray = pathname.split('/');
+	const breadcrumbPaths = [{
+		href: '/',
+		text: 'Home',
+	}, {
+		href: `/${pathnameArray[1]}`,
+		text: 'Blog',
+	}];
+
+	useEffect(() => {
+		const content = contentRef.current.innerText;
+		const contentReadTime = calculateReadTime(content);
+
+		setReadTime(contentReadTime);
+	}, []);
+
 	return (
-		<MainLayout display='grid'>
+		<MainLayout
+			display='grid'
+			seo={{
+				title: metadata.title,
+				description: metadata.description,
+				keywords: metadata.keywords,
+				pathUrl: pathname,
+				isArticle: true,
+				thumbnail: metadata.image,
+			}}
+		>
 			<section className='col-span-full'>
-				<ButtonLink
-					href='/blog'
-					value='Back to blog'
-					variant='empty'
-					icon={<MdChevronLeft size='20' />}
+				<Breadcrumb
+					paths={breadcrumbPaths}
+					current={metadata.title}
 				/>
 			</section>
+
 			{metadata.image && (
 				<section className='col-span-full'>
 					<div className='h-64 lg:h-96 relative'>
@@ -41,13 +76,13 @@ export function BlogLayout({ children, metadata, articles }: BlogLayoutType) {
 							src={metadata.image}
 							alt=''
 							fill={true}
-							className='object-cover'
-							priority={false}
-							sizes='100vw'
+							className='object-cover object-top'
+							sizes='(min-width: 1024px) 75vw, 100vw'
 						/>
 					</div>
 				</section>
 			)}
+
 			<section className='col-span-full text-center py-4 px-0 lg:p-10'>
 				<MainTitle>
 					{metadata.title}
@@ -57,20 +92,37 @@ export function BlogLayout({ children, metadata, articles }: BlogLayoutType) {
 					value='Josua Fernando Simanjuntak'
 					className='mt-4'
 				/>
-				<div className='my-2 flex flex-row gap-4 items-center justify-center'>
+				<div className='my-2 flex flex-row gap-2 items-center justify-center'>
 					<DisplayText
 						size='S'
 						value={metadata.date}
 					/>
+					{'·'}
 					<DisplayText
 						size='S'
-						value='8 minutes reading'
+						value={`${readTime} minutes reading`}
 					/>
 				</div>
 			</section>
-			<article className='col-span-full lg:col-span-9'>
+
+			<article ref={contentRef} className='main-content col-span-full lg:col-span-9'>
 				{children}
 			</article>
+
+			<aside className='col-span-full lg:col-span-3'>
+				<div className='flex flex-row lg:flex-col flex-wrap lg:flex-nowrap lg:items-center gap-4 lg:sticky lg:top-16'>
+					<RWebShare
+						data={{
+							title: metadata.title,
+							text: metadata.description,
+							url: `${sites.siteUrl}${pathname}`,
+						}}
+					>
+						<Button value='' variant='empty' icon={<MdShare size='24' />} iconOnly />
+					</RWebShare>
+				</div>
+			</aside>
+			
 			{articles && (
 				<section className='col-span-full'>
 					<SectionTitle>More Article</SectionTitle>

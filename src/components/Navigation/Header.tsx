f5f741ff610d7da64c7fdc9ae23f4febfbab4528
Aen Jojo <josuafernando999@gmail.com>
2023-02-24 22:51:16 +0700

    fix: update header
    * move navigation link mapping to outside of function
    
    * use static import for logo
    
    * fix mobile navigation menu

--- src/components/Navigation/Header.tsx
+++ src/components/Navigation/Header.tsx
@@ -1,33 +1,47 @@
 import { Button } from '#components/Button';
 import { ButtonLink } from '#components/Link';
 import { MdOutlineMenu, MdOutlineClose } from 'react-icons/md';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import clsx from 'clsx';
 import Image from 'next/image';
 import Link from 'next/link';
+import Logo from '#public/images/logo/aenjojo-full-color.svg';
+
+const NavigationList = [
+	'Resume',
+	'Projects',
+	'Blog'
+].map(element => (
+	<ButtonLink
+		key={element}
+		href={`/${element.toLowerCase()}`}
+		value={element}
+		variant='empty'
+		className='justify-center'
+	/>
+));
 
 export function Header() {
 	const [isOpen, setIsOpen] = useState(false);
 
-	const NavigationList = [
-		'Resume',
-		'Projects',
-		'Blog'
-	].map(element => (
-		<ButtonLink
-			key={element}
-			href={`/${element.toLowerCase()}`}
-			value={element}
-			variant='empty'
-			className='justify-center'
-		/>
-	))
+	useEffect(() => {
+		if (window) {
+			document.body.style.overflow = isOpen ? 'hidden' : 'auto';
+		}
+	}, [isOpen]);
 
 	return (
 		<header className='bg-primary-100 border-b border-b-primary-200 sticky top-0 z-10'>
 			<section className='container mx-auto flex flex-row justify-between px-2 py-1'>
-				<Link href='/' className='flex flex-row justify-center items-center focus-visible:outline-offset-4 focus-visible:outline-primary-300'>
-					<Image alt='' src='/images/logo/AenJojo - Full Colored.svg' width={144} height={32} />
+				<Link
+					href='/'
+					className='flex flex-row justify-center items-center focus-visible:outline-offset-4 focus-visible:outline-primary-300'
+				>
+					<Image
+						alt='main logo'
+						src={Logo}
+						className='w-32'
+					/>
 				</Link>
 				<nav className='hidden lg:flex lg:flex-row lg:gap-2 items-center justify-center'>
 					{NavigationList}
@@ -41,17 +55,19 @@ export function Header() {
 						}
 						iconOnly
 						variant={isOpen ? 'fill' : 'empty'}
-						onclick={() => setIsOpen(!isOpen)}
+						onClick={() => setIsOpen(!isOpen)}
 					/>
 				</section>
 			</section>
 			<nav
 				className={clsx([
-					'flex flex-col gap-2 lg:hidden w-10/12 mx-auto my-2 transition-all duration-300',
-					isOpen ? 'h-auto' : 'h-0 hidden'
+					'lg:hidden w-full bg-primary-100 transition-all duration-300 absolute',
+					isOpen ? 'h-screen' : 'h-0 invisible opacity-0'
 				])}
 			>
-				{NavigationList}
+				<div className='mx-auto my-2 w-10/12 flex flex-col gap-2'>
+					{NavigationList}
+				</div>
 			</nav>
 		</header>
 	);

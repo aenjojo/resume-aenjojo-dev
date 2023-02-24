e948492888a50bdb311d34ab7afc479bf6bf6c9c
Aen Jojo <josuafernando999@gmail.com>
2023-02-25 00:21:24 +0700

    feat: add skills data

---
+++ src/constants/skills.ts
@@ -0,0 +1,18 @@
+export type skillsType = {
+	name: string,
+	description: string,
+};
+
+export const skills: skillsType[] = [{
+	name: 'Web Development',
+	description: 'I have been learning about web development since 2017, but I started to create one that go for production mode in 2022',
+},{
+	name: 'JavaScript',
+	description: 'I have been using JS since 2017 and use it to develop a discord bot in 2018',
+},{
+	name: 'TypeScript',
+	description: 'I learned this language lately, since I need to know data types from modules I create when I rewrite my discord bot',
+},{
+	name: 'Next.js',
+	description: 'I started to learn this framework in 2021 and keep practicing to makes me familiar to the React environment',
+}];

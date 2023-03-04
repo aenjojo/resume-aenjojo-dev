7c77cc04be3303946918c1f2f026862bd6c9ce3b
Aen Jojo <josuafernando999@gmail.com>
2023-03-04 19:18:23 +0700

    fix: increase text color visibility

--- src/components/Chip/Chip.tsx
+++ src/components/Chip/Chip.tsx
@@ -5,7 +5,7 @@ export interface ChipPropsType {
 export function Chip({ value }: ChipPropsType) {
 	return (
 		<div
-			className='bg-primary-500 text-primary-100 rounded inline-block border border-primary-300 px-2 text-xs'
+			className='bg-primary-500 text-primary-50 rounded inline-block border border-primary-300 px-2 text-xs'
 		>
 			{value}
 		</div>

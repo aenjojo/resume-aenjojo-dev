77e5e66a3353697b595659425e80286618629959
Aen Jojo <josuafernando999@gmail.com>
2023-03-21 23:00:02 +0700

    chore(content): update educations data

--- src/constants/educations.ts
+++ src/constants/educations.ts
@@ -12,12 +12,16 @@ const educations: educationsType[] = [{
 	studyPlace: 'SoloLearn',
 	subject: 'Basic Programming',
 	startDate: 'February 2017',
-	endDate: 'December 2019',
-	description: 'I learn programming languages from basic',
+	endDate: 'Present',
+	description: 'I learned some programming, markup, and query languages from basic and programming concepts, especially in web development',
 	studies: [
 		'HTML',
 		'CSS',
 		'JavaScript',
+		'React',
+		'SQL',
+		'Responsive Web Design',
+		'Coding for Marketer',
 	],
 }, {
 	studyPlace: 'STIKOM Tunas Bangsa',
@@ -25,14 +29,26 @@ const educations: educationsType[] = [{
 	degree: 'Bachelor of Computer Science',
 	startDate: 'September 2018',
 	endDate: 'October 2022',
-	description: 'In college, I learn many things to get deeper knowledge on computer science',
+	description: 'In college, I learned many things to get deeper knowledge on computer science',
 	studies: [
-		'Algorithm',
+		'Algorithm & Programming',
 		'Data Structures',
 		'Databases',
 		'Web Development',
 		'Object-oriented Programming',
+		'Software Engineering',
 		'Artificial Intelligence',
+	],
+}, {
+	studyPlace: 'RevoU',
+	subject: 'Intro to Software Engineering',
+	startDate: 'February 2023',
+	endDate: 'March 2023',
+	description: 'In this mini course, I enrolled just to refresh my knowledge in basic web development',
+	studies: [
+		'HTML',
+		'CSS',
+		'JavaScript'
 	],
 }];
 

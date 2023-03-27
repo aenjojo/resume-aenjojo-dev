ebcd6eb48d90415f5cad61f6c528d72737fdf1f0
Aen Jojo <josuafernando999@gmail.com>
2023-03-28 00:12:31 +0700

    fix: use built-in `next/font` package

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import type { AppProps } from 'next/app';
-import { Spline_Sans, Spline_Sans_Mono } from '@next/font/google';
+import { Spline_Sans, Spline_Sans_Mono } from 'next/font/google';
 import { MDXProvider } from '@mdx-js/react';
 import { components } from '#components/mdx.index';
 import '#styles/globals.css';
@@ -7,11 +7,13 @@ import '#styles/globals.css';
 const splineSans = Spline_Sans({
 	subsets: ['latin', 'latin-ext'],
 	variable: '--font-spl-sans',
+	display: 'swap',
 });
 
 const splineMono = Spline_Sans_Mono({
 	subsets: ['latin', 'latin-ext'],
 	variable: '--font-spl-mono',
+	display: 'swap',
 });
 
 export default function MyApp({ Component, pageProps }: AppProps) {

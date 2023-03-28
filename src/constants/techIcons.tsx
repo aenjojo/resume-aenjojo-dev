1218eb736a692ff17760fa35d32d3c9f0d264407
Aen Jojo <josuafernando999@gmail.com>
2023-03-28 15:37:59 +0700

    feat: add more tech brand icons

--- src/constants/techIcons.tsx
+++ src/constants/techIcons.tsx
@@ -1,14 +1,18 @@
 import {
-	SiNextdotjs,
-	SiTailwindcss,
-	SiSvelte,
-	SiVite,
-	SiDiscord,
-	SiTypescript,
-	SiJavascript,
 	SiHtml5,
 	SiCss3,
+	SiJavascript,
+	SiTypescript,
 	SiMarkdown,
+	SiMdx,
+	SiReact,
+	SiNextdotjs,
+	SiSvelte,
+	SiTailwindcss,
+	SiDiscord,
+	SiVite,
+	SiVercel,
+	SiGithubpages,
 } from 'react-icons/si';
 
 export type techIconsType = {
@@ -17,48 +21,81 @@ export type techIconsType = {
 	icon: JSX.Element,
 };
 
-export const techIcons: techIconsType[] = [{
-	id: 'nextjs',
-	name: 'Next JS',
-	icon: <SiNextdotjs size='24' title='Next.js' fill='#000000' />,
-}, {
-	id: 'tailwind',
-	name: 'Tailwind CSS',
-	icon: <SiTailwindcss size='24' title='Tailwind css' fill='#06b6d4' />,
-}, {
-	id: 'svelte',
-	name: 'Svelte',
-	icon: <SiSvelte size='24' title='Svelte' fill='#ff3e00' />,
-}, {
-	id: 'sveltekit',
-	name: 'Svelte Kit',
-	icon: <SiSvelte size='24' title='Svelte kit' fill='#ff3e00' />,
-}, {
-	id: 'vite',
-	name: 'Vite',
-	icon: <SiVite size='24' title='Vite.js' fill='#646cff' />,
-}, {
-	id: 'discord.js',
-	name: 'Discord JS',
-	icon: <SiDiscord size='24' title='Discord.js' fill='#5865f2' />,
-}, {
-	id: 'js',
-	name: 'Javascript',
-	icon: <SiJavascript size='24' title='Javascript' fill='#f7df1e' />,
-}, {
-	id: 'ts',
-	name: 'Typescript',
-	icon: <SiTypescript size='24' title='Typescript' fill='#3178c6' />,
-}, {
-	id: 'html',
-	name: 'HTML',
-	icon: <SiHtml5 size='24' title='HTML' fill='#e34f26' />,
-}, {
-	id: 'css',
-	name: 'CSS',
-	icon: <SiCss3 size='24' title='CSS' fill='#1572b6' />,
-}, {
-	id: 'mdx',
-	name: 'MarkdownX',
-	icon: <SiMarkdown size='24' title='MDX' fill='#000000' />,
-}];
+export const techIcons: techIconsType[] = [
+	/**
+	 * languages
+	 */
+	{
+		id: 'html',
+		name: 'HTML',
+		icon: <SiHtml5 size='24' title='HTML' fill='#e34f26' />,
+	}, {
+		id: 'css',
+		name: 'CSS',
+		icon: <SiCss3 size='24' title='CSS' fill='#1572b6' />,
+	}, {
+		id: 'js',
+		name: 'Javascript',
+		icon: <SiJavascript size='24' title='Javascript' fill='#f7df1e' />,
+	}, {
+		id: 'ts',
+		name: 'Typescript',
+		icon: <SiTypescript size='24' title='Typescript' fill='#3178c6' />,
+	}, {
+		id: 'md',
+		name: 'Markdown',
+		icon: <SiMarkdown size='24' title='Markdown' fill='#000000' />,
+	}, {
+		id: 'mdx',
+		name: 'Markdown X',
+		icon: <SiMdx size='24' title='MDX' fill='#1B1F24' />,
+	},
+	/**
+	 * frameworks
+	 */
+	{
+		id: 'react',
+		name: 'React',
+		icon: <SiReact size='24' title='React' fill='#61DAFB'/>,
+	}, {
+		id: 'svelte',
+		name: 'Svelte',
+		icon: <SiSvelte size='24' title='Svelte' fill='#ff3e00' />,
+	}, {
+		id: 'nextjs',
+		name: 'Next JS',
+		icon: <SiNextdotjs size='24' title='Next.js' fill='#000000' />,
+	}, {
+		id: 'sveltekit',
+		name: 'Svelte Kit',
+		icon: <SiSvelte size='24' title='Svelte kit' fill='#ff3e00' />,
+	}, {
+		id: 'tailwind',
+		name: 'Tailwind CSS',
+		icon: <SiTailwindcss size='24' title='Tailwind CSS' fill='#06b6d4' />,
+	}, {
+		id: 'discordjs',
+		name: 'Discord JS',
+		icon: <SiDiscord size='24' title='Discord.js' fill='#5865f2' />,
+	},
+	/**
+	 * environments
+	 */
+	{
+		id: 'vite',
+		name: 'Vite',
+		icon: <SiVite size='24' title='Vite.js' fill='#646cff' />,
+	},
+	/**
+	 * deployments
+	 */
+	{
+		id: 'vercel',
+		name: 'Vercel',
+		icon: <SiVercel size='24' title='Vercel' fill='#000000'/>,
+	},  {
+		id: 'ghpages',
+		name: 'Github Pages',
+		icon: <SiGithubpages size='24' title='Github Pages' fill='#222222'/>,
+	},
+];

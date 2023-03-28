b09c460939804d776aef8ccbe211d5d43122f50f
Aen Jojo <josuafernando999@gmail.com>
2023-03-28 15:30:11 +0700

    chore: simplify the content path
    the path simplified because the contents are placed under `src`

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,7 @@ const { fontFamily } = require('tailwindcss/defaultTheme');
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-    './src/layouts/**/*.{js,ts,jsx,tsx}',
+    './src/**/*.{js,ts,jsx,tsx}',
   ],
   darkMode: 'class',
   theme: {

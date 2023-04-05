7bee61848546a93e47ef3719be4bbf8d29c72398
Aen Jojo <josuafernando999@gmail.com>
2023-04-05 23:48:28 +0700

    refactor: `textlink` follow tailwind guidelines

--- src/components/Link/TextLink.tsx
+++ src/components/Link/TextLink.tsx
@@ -1,13 +1,14 @@
 import clsx from 'clsx';
 import { BaseLink, type BaseLinkPropsType } from './BaseLink';
-import styles from '#styles/Link.module.css';
 
 export interface TextLinkPropsType extends BaseLinkPropsType {};
 
 export function TextLink({ href, value, openInNewTab, className }: TextLinkPropsType) {
 	const isExternalLink = Boolean(href) && !href.toString().startsWith('/') && !href.toString().startsWith('#');
 	const TextLinkStyle = clsx([
-		styles.base,
+		'transition-all duration-200 cursor-pointer font-medium underline decoration-2 underline-offset-4',
+        'text-primary-500 hover:text-secondary-500 hover:decoration-primary-500',
+        'focus:text-primary-600 active:text-primary-700 focus:outline-2 focus:outline-primary-300',
 		openInNewTab || isExternalLink ? 'inline-flex flex-nowrap items-center' : 'inline',
 		className,
 	]);

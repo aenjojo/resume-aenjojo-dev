ce2abd60f88460708eb62bc1210ba140204a0b95
aenjojo <josuafernando999@gmail.com>
2025-03-02 00:45:56 +0700

    feat: add breadcrumb

---
+++ src/lib/components/breadcrumb.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  export type Paths = {
+    name: string;
+    link: string;
+  }[];
+
+  interface Props {
+    paths: Paths;
+    current: string;
+  }
+
+  const { paths, current }: Props = $props();
+</script>
+
+<div class="space-x-1">
+  {#each paths as path}
+    <a href={path.link}>{path.name}</a>
+    <span>&rsaquo;</span>
+  {/each}
+  <span>{current}</span>
+</div>
